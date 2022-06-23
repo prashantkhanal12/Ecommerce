ab1b5cf54dfbd8a8067c9e34e2258fbd4abe2198
Prashant Khanal <prashantkhanal32@gmail.com>
2022-06-23 14:20:48 +0545

    chore(All): Project Creation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h3 className="text-center">
+        Switch to the feature/shipping details v1.0.0
+      </h3>
+    </div>
+  );
+};
+
+export default App;
